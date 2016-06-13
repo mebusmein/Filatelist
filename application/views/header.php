be81fb4c260487165e2730c522f521a5f3c68a3f
dennieb <dennieford@hotmail.com>
2016-06-13 13:30:22 +0200

    Added homepage, modified footer, header. Added favicon and modified CSS.

--- application/views/header.php
+++ application/views/header.php
@@ -7,9 +7,52 @@
 
     <meta name="description" content="">
 
-    <title>Filatelist</title>
+    <title>Phileon</title>
 
+    <!-- Loads favicon -->
+    <link rel="icon" type="image/ico" href="<?= base_url('assets/favicon/favicon.ico')?>">
+
+    <!-- Loads CSS files -->
     <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet">
-</head>
+    <link href="<?php echo base_url('assets/css/clean-blog.min.css') ?>" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+      <!-- Navigation -->
+      <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+          <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header page-scroll">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                  <!--            <a class="navbar-brand" href="index.html">Start Bootstrap</a>-->
+              </div>
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav navbar-right">
+                      <li>
+                          <a href="home">Home</a>
+                      </li>
+                      <li>
+                          <a href="about">Over Phileon</a>
+                      </li>
+                      <li>
+                          <a href="contact">Contact</a>
+                      </li>
+                  </ul>
+              </div>
+              <!-- /.navbar-collapse -->
+          </div>
+          <!-- /.container -->
+      </nav>
+  </head>
 <body>
