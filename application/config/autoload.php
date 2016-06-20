583a377df5c83ac886f0f2a5db6896ad10d816ff
dennieb <dennieford@hotmail.com>
2016-06-20 11:42:09 +0200

    Installed Community-auth succesfully. Remember to change database settings in application/config/database.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -40,7 +40,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$autoload['packages'] = array();
+$autoload['packages'] = array(APPPATH . 'third_party/community_auth/');
 
 
 /*
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database','session','tokens','Authentication');
 
 
 /*
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url','serialization','cookie');
 
 
 /*
@@ -104,7 +104,7 @@ $autoload['helper'] = array('url');
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('db_tables','authentication');
 
 
 /*
@@ -137,4 +137,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array();
+$autoload['model'] = array('auth_model');
---
+++ application/views/pages/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dennie
+ * Date: 20-Jun-16
+ * Time: 10:19 AM
+ */
